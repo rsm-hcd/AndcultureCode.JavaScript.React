3ee8bde654dd8d93b48968a3086f585b3b5a14c6
Brandon Scott <brandongregoryscott@gmail.com>
2020-03-27 12:43:41 -0400

    initial-port Porting over more of the route interfaces/utils

---
+++ src/interfaces/route-definition.ts
@@ -0,0 +1,13 @@
+import React from "react";
+import { RouteMap } from "interfaces/route-map";
+
+export interface RouteDefinition {
+    authRequired: boolean;
+    component: React.ComponentType;
+    exact?: boolean;
+    getComponent?: (location: any, cb: any) => void;
+    path: string;
+    routes: RouteMap;
+    // TODO: This may need to be decoupled from the package.
+    sidebar?: React.ComponentType;
+}
---
+++ src/interfaces/route-map.ts
@@ -0,0 +1,5 @@
+import { RouteDefinition } from "interfaces/route-definition";
+
+export interface RouteMap {
+    [route: string]: RouteDefinition;
+}
