521a8f303bd6978a84a7387f21a856889abb6b26
Brandon Scott <brandongregoryscott@gmail.com>
2020-04-20 19:26:05 -0400

    Update package-lock and add ts-jest global definition in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["./src/setupTests.ts"],
+    globals: {
+        "ts-jest": {
+            diagnostics: false,
+        },
+    },
 };
