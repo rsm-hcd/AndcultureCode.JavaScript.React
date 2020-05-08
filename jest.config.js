c2f9b4cfea3d6c81c20d98c13d9d2aad7186ac9b
Brandon Scott <brandongregoryscott@gmail.com>
2020-05-07 20:44:30 -0400

    Add clean script, exclude tests folder + setup file from build, ignore tests folder from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     preset: "ts-jest",
     collectCoverage: true,
+    coveragePathIgnorePatterns: ["<rootDir>/src/tests"],
     testEnvironment: "jsdom",
     modulePathIgnorePatterns: ["<rootDir>/dist"],
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
