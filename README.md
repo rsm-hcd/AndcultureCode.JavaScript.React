b5b07061b3e08d045700bfe334a0f26eb99ae7b7
Winton DeShong <contact@winton.me>
2020-05-15 14:46:16 -0400

    Reorganized re-exports in index; Improved readme

--- README.md
+++ README.md
@@ -1,11 +1,35 @@
 # AndcultureCode.JavaScript.React
 
-[![Build Status](https://travis-ci.org/brandongregoryscott/AndcultureCode.JavaScript.React.svg?branch=master)](https://travis-ci.org/brandongregoryscott/AndcultureCode.JavaScript.React)
-[![codecov](https://codecov.io/gh/brandongregoryscott/AndcultureCode.JavaScript.React/branch/master/graph/badge.svg)](https://codecov.io/gh/brandongregoryscott/AndcultureCode.JavaScript.React)
+[![Build Status](https://travis-ci.org/AndcultureCode/AndcultureCode.JavaScript.React.svg?branch=master)](https://travis-ci.org/AndcultureCode/AndcultureCode.JavaScript.React)
+[![codecov](https://codecov.io/gh/AndcultureCode/AndcultureCode.JavaScript.React/branch/master/graph/badge.svg)](https://codecov.io/gh/AndcultureCode/AndcultureCode.JavaScript.React)
 
 Common patterns, functions, etc... used when building react applications
 
 ## Getting started
+
+This package is installed via npm or yarn
+
+```shell
+# npm
+npm install --save-dev andculturecode-javascript-react
+
+# yarn
+yarn add andculturecode-javascript-react --dev
+```
+
+From there you can import the variety of modules.
+
+```typescript
+import {
+    AuthenticatedRoute,
+    NestedRoute,
+    NestedRoutes,
+    ErrorType,
+    PagedResult,
+    CoreUtils,
+    ResultRecord,
+} from "andculturecode-javascript-react";
+```
 
 ## Troubleshooting
 
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,68 @@
-export * from "components/routing/nested-route";
-export * from "components/routing/authenticated-route";
-export * from "components/routing/nested-routes";
-export * from "components/routing/nested-routes-by-property";
-export * from "components/routing/redirects";
-export * from "enumerations/error-type";
-export * from "interfaces/redirect-definition";
-export * from "interfaces/result-error";
-export * from "interfaces/result";
-export * from "interfaces/route-definition";
-export * from "interfaces/route-map";
-export * from "interfaces/service-response";
-export * from "interfaces/paged-result";
+// -----------------------------------------------------------------------------------------
+// #region Components
+// -----------------------------------------------------------------------------------------
+
+export * from "./components/routing/authenticated-route";
+export * from "./components/routing/nested-route";
+export * from "./components/routing/nested-routes";
+export * from "./components/routing/nested-routes-by-property";
+export * from "./components/routing/redirects";
+
+//#endregion Components
+
+// -----------------------------------------------------------------------------------------
+// #region Enumeratinos
+// -----------------------------------------------------------------------------------------
+
+export * from "./enumerations/error-type";
+
+//#endregion Enumerations
+
+// -----------------------------------------------------------------------------------------
+// #region Interfaces
+// -----------------------------------------------------------------------------------------
+
+export * from "./interfaces/paged-result";
+export * from "./interfaces/redirect-definition";
+export * from "./interfaces/result-error";
+export * from "./interfaces/result";
+export * from "./interfaces/route-definition";
+export * from "./interfaces/route-map";
+export * from "./interfaces/service-response";
+
+//#endregion Interfaces
+
+// -----------------------------------------------------------------------------------------
+// #region Services
+// -----------------------------------------------------------------------------------------
+
+export * from "./services/service-factory";
+
+//#endregion Services
+
+// -----------------------------------------------------------------------------------------
+// #region Utilities
+// -----------------------------------------------------------------------------------------
+
+export * from "./utilities/collection-utils";
+export * from "./utilities/core-utils";
+export * from "./utilities/route-utils";
+
+//#endregion Utilities
+
+// -----------------------------------------------------------------------------------------
+// #region Vendor
+// -----------------------------------------------------------------------------------------
+
 export * from "react-router-dom";
-export * from "services/service-factory";
-export * from "utilities/collection-utils";
-export * from "utilities/core-utils";
-export * from "utilities/route-utils";
-export * from "view-models/result-error-record";
-export * from "view-models/result-record";
+
+//#endregion Vendor
+
+// -----------------------------------------------------------------------------------------
+// #region ViewModels
+// -----------------------------------------------------------------------------------------
+
+export * from "./view-models/result-error-record";
+export * from "./view-models/result-record";
+
+//#endregion ViewModels
