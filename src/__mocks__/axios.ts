0356f1a332ab0b639292b83df5de42ffc5b4823e
Winton DeShong <contact@winton.me>
2020-05-20 14:38:12 -0400

    Using v0.0.4 of Core

--- src/__mocks__/axios.ts
+++ src/__mocks__/axios.ts
@@ -1,17 +1,1 @@
-export default {
-    defaults: {
-        headers: {
-            post: {},
-            put: {},
-        },
-    },
-    delete: jest.fn(() => Promise.resolve({ data: {} })),
-    get: jest.fn(() => Promise.resolve({ data: {} })),
-    interceptors: {
-        response: {
-            use: jest.fn(),
-        },
-    },
-    post: jest.fn(() => Promise.resolve({ data: {} })),
-    put: jest.fn(() => Promise.resolve({ data: {} })),
-};
+export * from "andculturecode-javascript-core/dist/__mocks__/axios";
