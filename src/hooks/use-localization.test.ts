2c6fe7910fb798fb3ad45783821c408bde9c2aa1
Winton DeShong <contact@winton.me>
2020-06-26 11:23:23 -0400

    Stub in useLocalization test file

---
+++ src/hooks/use-localization.test.ts
@@ -0,0 +1,7 @@
+import { Factory } from "rosie";
+import { FactoryType } from "../tests/factories/factory-type";
+import "jest-extended";
+
+describe("useLocalization", () => {
+    test.skip("TODO", () => {});
+});
