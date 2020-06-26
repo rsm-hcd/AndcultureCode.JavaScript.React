fbdd02a1bb30b9fd147fa63fcdf9bfe3c1783a67
Winton DeShong <contact@winton.me>
2020-06-26 14:45:51 -0400

    Unit testing use-localization hook

--- src/hooks/use-localization.test.ts
+++
@@ -1,7 +0,0 @@
-import { Factory } from "rosie";
-import { FactoryType } from "../tests/factories/factory-type";
-import "jest-extended";
-
-describe("useLocalization", () => {
-    test.skip("TODO", () => {});
-});
