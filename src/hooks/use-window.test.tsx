260adc9ad65ccd6973e0b7d0622d611e409bc37b
Michael Tyson <michaeltyson@outlook.com>
2020-10-06 08:31:44 -0400

    Issue #24 - Add useWindow tests

--- src/hooks/use-window.test.tsx
+++ src/hooks/use-window.test.tsx
@@ -1,3 +1,34 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useWindow } from "./use-window";
+
+const DEFAULT_WIDTH: number = 1024;
+const DEFAULT_HEIGHT: number = 768;
+
 describe("useWindow", () => {
-    test.skip("TODO - https://github.com/AndcultureCode/AndcultureCode.JavaScript.React/issues/24", () => {});
+    beforeEach(() => {
+        window.resizeTo(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    });
+
+    test("returns width and height of window", async () => {
+        // Arrange & Act
+        const { result } = renderHook(() => useWindow());
+
+        // Assert
+        expect(result.current.width).toBe(DEFAULT_WIDTH);
+        expect(result.current.height).toBe(DEFAULT_HEIGHT);
+    });
+
+    test("when window resize event is triggered, returns new width and height of window", async () => {
+        // Arrange
+        const { result } = renderHook(() => useWindow());
+        const windowWidth = DEFAULT_WIDTH + 1;
+        const windowHeight = DEFAULT_HEIGHT + 1;
+
+        // Act
+        window.resizeTo(windowWidth, windowHeight);
+
+        // Assert
+        expect(result.current.width).toBe(windowWidth);
+        expect(result.current.height).toBe(windowHeight);
+    });
 });
