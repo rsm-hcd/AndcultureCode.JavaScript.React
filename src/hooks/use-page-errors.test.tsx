711ccbd0bcab8cd93cf3d081818039dc30a9d0bf
Michael Tyson <michaeltyson@outlook.com>
2021-01-16 15:24:46 -0500

    add usePageErrors tests

--- src/hooks/use-page-errors.test.tsx
+++ src/hooks/use-page-errors.test.tsx
@@ -1,3 +1,67 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { usePageErrors } from "./use-page-errors";
+import { ResultRecord } from "andculturecode-javascript-core";
+
 describe("usePageErrors", () => {
-    test.skip("TODO - https://github.com/AndcultureCode/AndcultureCode.JavaScript.React/issues/22", () => {});
+    test("returns empty array", async () => {
+        // Arrange & Act
+        const { result } = renderHook(() => usePageErrors());
+
+        // Assert
+        expect(result.current.pageErrors).toBeEmpty();
+    });
+
+    describe("handlePageLoadError", () => {
+        test("when error is string, pageErrors returns array", async () => {
+            // Arrange
+            const errorMessage = "Error Message";
+            const { result } = renderHook(() => usePageErrors());
+
+            // Act
+            act(() => {
+                result.current.handlePageLoadError(errorMessage);
+            });
+
+            // Assert
+            expect(result.current.pageErrors.length).toBe(1);
+            expect(result.current.pageErrors[0]).toBe(errorMessage);
+        });
+
+        test("when error is ResultRecord, pageErrors returns array", async () => {
+            // Arrange
+            const errorMessage = "Error Message";
+            const { result } = renderHook(() => usePageErrors());
+
+            // Act
+            act(() => {
+                const error = {
+                    listErrorMessages: () => [errorMessage],
+                } as ResultRecord<any>;
+
+                result.current.handlePageLoadError(error);
+            });
+
+            // Assert
+            expect(result.current.pageErrors.length).toBe(1);
+            expect(result.current.pageErrors[0]).toBe(errorMessage);
+        });
+    });
+
+    describe("resetPageErrors", () => {
+        test("when executed, pageErrors returns empty array", async () => {
+            // Arrange
+            const { result } = renderHook(() => usePageErrors());
+            act(() => {
+                result.current.handlePageLoadError("Error Message");
+            });
+
+            // Act
+            act(() => {
+                result.current.resetPageErrors();
+            });
+
+            // Assert
+            expect(result.current.pageErrors).toBeEmpty();
+        });
+    });
 });
