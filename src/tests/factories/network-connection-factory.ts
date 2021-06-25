2d2506b5e895b7b91f6ffe167c7f3179ef308483
Michael Tyson <michaeltyson@outlook.com>
2021-06-25 07:15:33 -0400

    remove rtt attrinute from NetworkConnectionFactory

--- src/tests/factories/network-connection-factory.ts
+++ src/tests/factories/network-connection-factory.ts
@@ -1,6 +1,5 @@
 import { NetworkConnection } from "andculturecode-javascript-core";
 import { Factory } from "rosie";
-import faker from "faker";
 
 // -----------------------------------------------------------------------------------------
 // #region Factory
@@ -9,7 +8,6 @@ import faker from "faker";
 export const NetworkConnectionFactory = Factory.define<NetworkConnection>(
     "NetworkConnection"
 )
-    .attr("rtt", rttFactory)
     .attr("isOnline", true)
     .attr("addEventListener", eventListenerFactory)
     .attr("removeEventListener", eventListenerFactory);
@@ -22,15 +20,6 @@ export const NetworkConnectionFactory = Factory.define<NetworkConnection>(
 
 function eventListenerFactory() {
     return () => {};
-}
-
-/**
- * Returns a random estimated effective round-trip time of the current connection,
- * rounded to the nearest multiple of 25 milliseconds. Values will range from 25 - 1000 ms.
- * https://wicg.github.io/netinfo/#networkinformation-interface
- */
-function rttFactory() {
-    return 25 * (faker.random.number(39) + 1);
 }
 
 // #endregion Functions
